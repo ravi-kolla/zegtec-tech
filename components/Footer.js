f4c121d0fa86a23d5e9b7fb2e7b543e61998889a
Ravi Kolla <ravi.kolla@outlook.com>
2022-12-21 12:58:25 -0500

    footer error fix

--- components/Footer.js
+++ components/Footer.js
@@ -6,38 +6,38 @@ export const Footer = ({ footer, settings }) => {
   console.log(footer.data.links);
   console.log(footer.data.otherlinks);
   return (
-    <div class="bg-gray-100 pt-5">
-       <div class="max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
+    <div class="pt-5 border-t-4 border-lilac-color">
+       <div class="sh-lilac shadow-xl max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
          <div class="p-5">
          <PrismicLink href="/" className="pt-2 text-xl font-semibold tracking-tight max-h-52">
            <PrismicNextImage field={footer.data.logo}/>
          </PrismicLink>
          </div>
           <div class="p-5">
-             <div class="text-sm uppercase text-indigo-600 font-bold">
+             <div class="text-sm uppercase lilac-color font-bold">
               <PrismicRichText field={footer.data.firstColumnTitle} />
              </div>
                {footer.data?.links.map((item) => (
-                 <a key={item.label} class="my-3 block" href={item.link.url}>{item.label}</a>
+                 <PrismicLink key={item.label} className="my-3 block" href={item.link.url}>{item.label}</PrismicLink>
                ))}
           </div>
           <div class="p-5">
-          <div class="text-sm uppercase text-indigo-600 font-bold">{footer.data.headerSecond}</div>
+          <div class="text-sm uppercase lemon-color font-bold">{footer.data.headerSecond}</div>
              {footer.data?.otherlinks.map((item) => (
-               <a key={item.label} class="my-3 block" href={item.link.url}>{item.label}</a>
+               <PrismicLink key={item.label} className="my-3 block" href={item.link.url}>{item.label}</PrismicLink>
              ))}
           </div>
           <div class="p-5">
-             <div class="text-sm uppercase text-indigo-600 font-bold">Contact us</div>
-             <a class="my-3 block" href="/#">XXX XXXX, Floor 4 San Francisco, CA</a>
-             <a class="my-3 block" href="/#">contact@company.com </a>
+             <div class="text-sm uppercase blue-brand font-bold">Contact us</div>
+             <p class="my-3 block" href="/#">XXX XXXX, Floor 4 San Francisco, CA</p>
+             <p class="my-3 block" href="/#">contact@company.com </p>
           </div>
        </div>
-       <div class="bg-gray-100 pt-2">
-          <div class="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col max-w-screen-lg items-center">
+       <div class="pt-2">
+          <div class="flex pb-5 px-3 m-auto pt-5 text-gray-800 text-sm flex-col max-w-screen-lg items-center">
              <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
-                <a href="/#" class="w-6 mx-1"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="/#" class="w-6 mx-1"><i class="fa-brands fa-twitter"></i></a>
+                <a href="https://linkedin.com" class="w-6 mx-1"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://twitter.com" class="w-6 mx-1"><i class="fa-brands fa-twitter"></i></a>
              </div>
              <div class="my-5">{footer.data.copyright}</div>
           </div>
